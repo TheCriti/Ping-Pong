dafd0b74679b05a1db87ef42b6ba3fd630467c63
TheCriti <135810240+TheCriti@users.noreply.github.com>
2023-06-08 09:39:41 +0300

    Update ping_pong.py
    v1.1:
    Racket move
    Add ball

--- ping_pong.py
+++ ping_pong.py
@@ -1,13 +1,9 @@
-#Создай собственный Шутер!
-
 from pygame import *
-from random import *
-from time import time as timer
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, w, h):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.image = transform.scale(image.load(player_image), (w, h))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -31,19 +27,16 @@ class Player(GameSprite):
             
 class Enemy(GameSprite):
     def update(self):
-        if self.rect.y <= 500:
-            self.rect.y += self.speed
-        else:
-            self.rect.y = 0
-            self.rect.x = randint(0, 650)
-            self.speed = randint(1, 3)
-
+        self.speed = 2
+        self.rect.y = 0
+        self.rect.x = 200
 w = 516
 h = 516
+speed = 3
 
-rocket1 = Player('rocket.png', 5, 5, 5)
-rocket2 = Player('rocket.png', 500, 5, 5)
-ball = Enemy('ball.png', 129, -10, 3)
+rocket1 = Player('rocket.png', 0, 0, 5, 100, 100)
+rocket2 = Player('rocket.png', 408, 5, 5, 100, 100)
+ball = Enemy('ball.png', 129, -10, 3, 40, 40)
 
 window = display.set_mode((516, 516))
 display.set_caption('Пинг-понг')
@@ -65,5 +58,15 @@ while game:
             game = False
     if not finish:
         window.blit(background, (0,0))
+        rocket1.update1()
+        rocket2.update2()
+        ball.update()
+        rocket1.reset()
+        rocket2.reset()
+        ball.rect.y += ball.speed
+        ball.rect.x += ball.speed
+        if sprite.collide_rect(rocket1, ball) or sprite.collide_rect(rocket2, ball):
+            ball.speed *= -1
+        ball.reset()
         display.update()
     clock.tick(FPS)
